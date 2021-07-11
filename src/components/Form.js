407b4796ea095544e84e49d4a84c738b62dfe386
nasiphivinqishe@gmail.com <nasiphivinqishe@gmail.com>
2021-07-11 15:41:54 +0200

    Refactoring - separated the input list to contants

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { myInputFieldToChooseFrom } from "../constants"
 const useStyles = makeStyles((theme) =>
     createStyles({
         formGroup: {
@@ -16,36 +17,11 @@ const Form = () => {
     const [style, setStyle] = useState([])
     const [selectOptions, setSelectOptions] = useState([])
 
-    const myInputFieldToChooseFrom = [
-        { key: "default", inputType: "text", value: "<input type='text' value=''/>", label: "", description: "Select input.", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "text", value: "<input type='text' placeholder='Enter text here...' value=''/>", label: "", description: "Text Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "button", value: "<input type='button' value='Button'/>", label: "", description: "Button", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "file", value: "<input type='file'value=''/>", label: "", description: "File Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "password", value: "<input type='password' placeholder='Enter password here...' value=''/>", label: "", description: "Password Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "number", value: "<input type='number' placeholder='Enter number here...' value=''/>", label: "", description: "Number Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "checkbox", value: "<input type='checkbox'value=''/>", label: "", description: "Checkbox Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "color", value: "<input type='color'value=''/>", label: "", description: "Color Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "date", value: "<input type='date'value=''/>", label: "", description: "Date Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "datetime-local", value: "<input type='datetime-local'value=''/>", label: "", description: "Datetime-local Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "email", value: "<input type='email'value=''/>", label: "", description: "Email Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "hidden", value: "<input type='hidden'value=''/>", label: "", description: "Hidden Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "month", value: "<input type='month'value=''/>", label: "", description: "Month Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "image", value: "<input type='image'value=''/>", label: "", description: "Image Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "radio", value: "<input type='radio'value=''/>", label: "", description: "Radio Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "reset", value: "<input type='reset'value=''/>", label: "", description: "Reset Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "search", value: "<input type='search'value=''/>", label: "", description: "Search Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "submit", value: "<input type='submit'value=''/>", label: "", description: "Submit Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "tel", value: "<input type='tel'value=''/>", label: "", description: "Tel Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "time", value: "<input type='time'value=''/>", label: "", description: "Time Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "url", value: "<input type='url'value=''/>", label: "", description: "Url Input", style: [{ name: "", value: "" }] },
-        { key: "input", inputType: "week", value: "<input type='week'value=''/>", label: "", description: "Week Input", style: [{ name: "", value: "" }] },
-        { key: "textarea", inputType: "textarea", value: "<textarea type='text' placeholder='Enter text here...' value=''/></textarea>", label: "", description: "Text Area", style: [{ name: "", value: "" }] },
-        { key: "select", inputType: "select", value: "<select></select>", label: "", description: "Select", style: [{ name: "", value: "" }] }
-    ]
+
 
     const handleOnChangeChoosingInputToAdd = (e) => {
-        var key = e.target.value
-        var choosendInput = myInputFieldToChooseFrom.find(input => input.key == key)
+        var inputType = e.target.value
+        var choosendInput = myInputFieldToChooseFrom.find(input => input.inputType == inputType)
         updateCurrentInputCustomizing(choosendInput)
     }
 
@@ -159,8 +135,8 @@ const Form = () => {
 
                     {myInputFieldToChooseFrom.map((input, index) => {
                         if (index == 0)
-                            return <option key={index} selected="true" value={input.key} disabled>{input.description}</option>
-                        return <option key={index} value={input.key}>{input.description}</option>
+                            return <option key={index} selected="true" value={input.inputType} disabled>{input.description}</option>
+                        return <option key={index} value={input.inputType}>{input.description}</option>
                     })}
 
                 </select>
@@ -241,12 +217,25 @@ const Form = () => {
 
                         if (input.key == "input") {
 
+                            if (input.inputType == 'button' || input.inputType == 'submit') {
+                                return <div key={index} className="form-group">
+                                    <input
+                                        type={input.inputType}
+                                        value={input.label}
+                                        style={input.style?.reduce((agg, val) => {
+                                            agg[val.name] = val.value;
+                                            return agg;
+                                        }, {})}
+                                    />
+                                </div>
+                            }
+
                             return (
                                 <div key={index} className="form-group">
                                     <label><b>{input.label}</b></label>
 
-
-                                    <input type="text"
+                                    <input
+                                        type={input.inputType}
                                         style={input.style?.reduce((agg, val) => {
                                             agg[val.name] = val.value;
                                             return agg;
